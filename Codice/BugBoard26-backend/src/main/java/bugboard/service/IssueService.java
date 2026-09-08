4c20c877e1d6abd992b218fed3b1e67f2a79622a
galetizia <ga.letizia@studenti.unina.it>
2026-09-08 14:48:00 +0200

    implementato il caricamento dell'immagine nel DB

--- Codice/BugBoard26-backend/src/main/java/bugboard/service/IssueService.java
+++ Codice/BugBoard26-backend/src/main/java/bugboard/service/IssueService.java
@@ -1,11 +1,15 @@
 package bugboard.service;
 
 import bugboard.enums.StatoIssue;
+import bugboard.enums.TipoIssue;
+import bugboard.model.Attachment;
 import bugboard.model.AuthUser;
 import bugboard.model.Issue;
 import bugboard.repository.IssueRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,15 +21,29 @@ public class IssueService {
         this.issueRepository = issueRepository;
     }
 
-    public void createIssue(String titolo, String descrizione, String priorita) {
+    public void createIssue(String titolo, String descrizione, TipoIssue tipologia , String priorita, MultipartFile file) {
         Issue issue = new Issue();
 
         issue.setTitolo(titolo);
         issue.setDescrizione(descrizione);
+        issue.setTipo(tipologia);
 
         if(priorita!=null && !priorita.isBlank()) issue.setPriorita(priorita);
 
+        issue.setStato(StatoIssue.TO_DO);
 
+        if(file!=null && !file.isEmpty()){
+            try{
+                Attachment allegato = new Attachment();
+                allegato.setNome(file.getOriginalFilename());
+                allegato.setTipo(file.getContentType());
+                allegato.setContenuto(file.getBytes());
+
+                issue.setAllegato(allegato);
+            } catch(IOException e){
+                throw new RuntimeException("Errore nel caricamento dell'immagine", e);
+            }
+        }
         //stato to-do di default
         issueRepository.save(issue);
     }
